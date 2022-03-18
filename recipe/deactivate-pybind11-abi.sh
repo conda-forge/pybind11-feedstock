06c0affd3bab7550a702fac6a3685cd997b7bc74
Matthew R. Becker <becker.mr@gmail.com>
2022-03-18 09:01:10 -0500

    WIP looser ABI compat for pybind11

---
+++ recipe/deactivate-pybind11-abi.sh
@@ -0,0 +1,28 @@
+
+function backup_and_append() {
+    local way=$1
+    local envvar=$2
+
+    if [[ ${way} == "activate" ]]; then
+        eval oldval="\$${envvar}"
+
+        eval "export PYBIND11_ABI_BACKUP_${envvar}=\"${oldval}\""
+        if [[ ! ${oldval} ]]; then
+            eval "export ${envvar}=\"-DPYBIND11_COMPILER_TYPE= -DPYBIND11_BUILD_ABI=\""
+        else
+            eval "export ${envvar}=\"${oldval} -DPYBIND11_COMPILER_TYPE= -DPYBIND11_BUILD_ABI=\""
+        fi
+    else
+        eval backval="\$PYBIND11_ABI_BACKUP_${envvar}"
+
+        if [[ ! ${backval} ]]; then
+            eval "unset ${envvar}"
+        else
+            eval "export ${envvar}=\"${backval}\""
+        fi
+        eval "unset PYBIND11_ABI_BACKUP_${envvar}"
+    fi
+}
+
+backup_and_append deactivate "CXXFLAGS"
+backup_and_append deactivate "DEBUG_CXXFLAGS"
