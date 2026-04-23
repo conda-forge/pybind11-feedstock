176eba36e8bd914f4689de9fa5d283f7232ef2a0
Axel Huebl <axel.huebl@plasma.ninja>
2026-04-22 21:46:35 -0700

    Win: Ninja Generator
    Try to overcome:
    ```
     │ │   -- Building for: Visual Studio 17 2022
     │ │   CMake Error at CMakeLists.txt:38 (project):
     │ │     Generator
     │ │       Visual Studio 17 2022
     │ │     could not find any instance of Visual Studio.
    ```

--- recipe/build-pybind11-global.bat
+++ recipe/build-pybind11-global.bat
@@ -8,6 +8,7 @@ if errorlevel 1 exit 1
 
 :: Build with generated pyprojectglobal.toml
 move /Y pyprojectglobal.toml pyproject.toml
+set "CMAKE_GENERATOR=Ninja"
 :: There are several required flags, like --no-deps, but conda-build nicely sets them for us
 :: https://github.com/conda/conda-build/blob/d75ef9c66dbcc832d8d96f33a2aecfe893266e8c/conda_build/build.py#L2517-L2527
 %PYTHON% -m pip install . -vv --no-build-isolation --config-settings=wheel.install-dir=/data/Library
