cf902d7e56076100639175e15f478bd1f2852293
Axel Huebl <axel.huebl@plasma.ninja>
2026-04-22 23:39:46 -0700

    Remove unused CMake generator platform and toolset

--- recipe/build-pybind11.bat
+++ recipe/build-pybind11.bat
@@ -1,6 +1,4 @@
 set "CMAKE_GENERATOR=Ninja"
-set "CMAKE_GENERATOR_PLATFORM="
-set "CMAKE_GENERATOR_TOOLSET="
 :: There are several required flags, like --no-deps, but conda-build nicely sets them for us
 :: https://github.com/conda/conda-build/blob/d75ef9c66dbcc832d8d96f33a2aecfe893266e8c/conda_build/build.py#L2517-L2527
 %PYTHON% -m pip install . -vv --no-build-isolation
